4796479840bae706175d087dedb0157267570fb8
Eugene <mosgate@gmail.com>
2025-08-06 14:39:37 +0300

    Revert "feat: добавить ввод категорий затрат"

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import '@ant-design/v5-patch-for-react-19';
-import '@ant-design/compatible';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
