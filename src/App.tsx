9be39cc37fee7b3a94031a160499ba98191a6b1c
Eugene <mosgate@gmail.com>
2025-08-27 17:03:15 +0300

    Revert "feat: add portal logo above menu"

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ import Statuses from './pages/admin/Statuses'
 import Disk from './pages/admin/Disk'
 import PortalHeader from './components/PortalHeader'
 import TestTableStructure from './pages/TestTableStructure'
-import logoLight from './logo_light.svg'
 
 const { Sider, Content } = Layout
 
@@ -442,14 +441,7 @@ const App = ({ isDark, toggleTheme }: AppProps) => {
           collapsed={collapsed}
           onCollapse={setCollapsed}
         >
-          <div
-            style={{
-              width: '100%',
-              marginBottom: 16,
-            }}
-          >
-            <img src={logoLight} alt="BlueprintFlow logo" style={{ width: '100%', height: 'auto', display: 'block' }} />
-          </div>
+          <div style={{ height: 64 }} />
           <Menu
             theme={isDark ? 'dark' : 'light'}
             mode="inline"
